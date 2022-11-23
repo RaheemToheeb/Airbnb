79ffffc7e732b58f9f12af0a85d1ee89d75ba39e
EngrRaheemT <98194041+RaheemToheeb@users.noreply.github.com>
2022-11-24 00:31:32 +0100

    PES-36 Changes make database

--- server/Utils/Database.js
+++ server/Utils/Database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 url = "mongodb://localhost/Airnnb";
 mongoose.connect(url).then((req, res) => {
   try {
-    console.log("Data base connected");
+    console.log("Data base connect");
   } catch (error) {
     console.log("Cannot connet to Data base");
   }
